c80481a62ae421993790c5756637bc79cf8e3d31
RGPin <157803400+RGPin@users.noreply.github.com>
2025-08-31 08:14:39 +0800

    Create home page

--- src/home.js
+++ src/home.js
@@ -1,6 +1,23 @@
+import bgImage from "./images/pexels-brettjordan-825661.jpg";
+
 export function homePage(content) {
+    document.body.style.background = `
+    linear-gradient(rgba(0,0,0,0.2), 
+    rgba(0,0,0,0.2)),
+    url(${bgImage})
+    `;
+    document.body.style.backgroundSize = "cover";
     content.textContent = "";
-    const test = document.createElement("p");
-    test.textContent = "Home page";
-    content.appendChild(test);
+    const home = document.createElement("div");
+    const name = document.createElement("h1");
+    const tagline = document.createElement("p");
+
+    home.classList.add("home");
+
+    name.textContent = "La Tavola";
+    tagline.textContent = "Where Flavor Meets Tradition";
+
+    home.appendChild(name);
+    home.appendChild(tagline);
+    content.appendChild(home);
 }
